a59eaabdaaa4f8b52a741db5c33c21fed43b3cce
Michele Fabbri <michele.fabbri@internationalbrainlab.org>
2022-06-01 11:24:11 +0100

    removing packaging requirement

--- install.py
+++ install.py
@@ -1,24 +1,39 @@
+import os
 import subprocess
-from packaging.version import parse
-
 
 # TODO: logging
+ON_WINDOWS = True if os.name == "nt" else False
 
 
 def check_dependencies():
     """Check required dependencies are installed"""
     print("Checking for dependencies...")
+    environment_vars = str(subprocess.check_output([os.environ["CONDA_EXE"], "list", "-n", "base", "--json"]))
+    if "conda" not in environment_vars:
+        print("--- Anaconda not found in environment, please follow the README instructions for repo dependencies ---")
+        print(environment_vars)
+        exit(0)
     try:
-        conda_version = str(subprocess.check_output(["conda", "--version"])).split(" ")[1].split("\\n")[0]
-        conda_version = conda_version.strip("\\r") if "\\r" in conda_version else None  # Windows specific annoyance
-    except subprocess.CalledProcessError:
+        conda_version = str(subprocess.check_output(["conda", "--version"], shell=True)).split(" ")[1].split(".") if ON_WINDOWS \
+            else conda_version = str(subprocess.check_output(["conda", "--version"])).split(" ")[1].split(".")
+    except subprocess.CalledProcessError or FileNotFoundError:
         print("Anaconda call failed, check if Anaconda is installed.")
         raise
-    if parse(conda_version) < parse("4.0.0"):
-        print("conda version is less than v4, update is required...")
-        # conda update -q -y -n base -c defaults conda
-        exit(1)
-    print("All dependencies OK.")
+    conda_major_version = int(conda_version[0])
+    conda_minor_version = int(conda_version[1])
+    if conda_major_version < 4:
+        print("conda version is less than v4, manual removal and re-installation is required...")
+        exit(0)
+    elif conda_major_version == 4 and conda_minor_version < 10:
+        print("Attempting to update Anaconda...")
+        try:
+            subprocess_cmd = ["conda", "update", "--quiet", "--yes", "--name", "base", "--channel", "defaults", "conda"]
+            subprocess.check_output(subprocess_cmd, shell=True) if ON_WINDOWS else subprocess.check_output(subprocess_cmd)
+        except subprocess.CalledProcessError:
+            print("Anaconda update failed, check if Anaconda is installed.")
+            raise
+    else:
+        print("All dependencies OK.")
 
 
 def check_or_create_iblenv():
@@ -29,7 +44,8 @@ def check_or_create_iblenv():
     else:
         print("iblenv does not currently exist, creating it now...")
         try:
-            subprocess.check_call(["conda", "create", "-q", "-y", "--name", "iblenv", "python=3.8"])
+            subprocess_cmd = ["conda", "create", "-q", "-y", "--name", "iblenv", "python=3.8"]
+            subprocess.check_call(subprocess_cmd, shell=True) if ON_WINDOWS else subprocess.check_call(subprocess_cmd)
             print("Environment created")
         except subprocess.CalledProcessError:
             print("Anaconda creation of the iblenv environment failed.")
@@ -40,9 +56,11 @@ def pip_install_packages():
     """Use pip to install packages listed in requirements.txt"""
     print("Ensuring pip is up to date and using it to install packages in the requirements.txt file...")
     try:
-        subprocess.check_call(["conda", "run", "--name", "iblenv", "python", "-m", "pip", "install", "--upgrade", "pip"])
-        subprocess.check_call(
-            ["conda", "run", "--name", "iblenv", "python", "-m", "pip", "install", "--requirement", "requirements.txt"])
+        sp_upgrade_cmd = ["conda", "run", "--name", "iblenv", "python", "-m", "pip", "install", "--upgrade", "pip"]
+        sp_run_cmd = ["conda", "run", "--name", "iblenv", "python", "-m", "pip", "install", "--requirement", "requirements.txt"]
+
+        subprocess.check_call(sp_upgrade_cmd, shell=True) if ON_WINDOWS else subprocess.check_call(sp_upgrade_cmd)
+        subprocess.check_call(sp_run_cmd, shell=True) if ON_WINDOWS else subprocess.check_call(sp_run_cmd)
     except subprocess.CalledProcessError:
         print("pip installation could not be completed")
         raise
